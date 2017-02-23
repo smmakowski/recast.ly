1ae7c074af73a6878eaa11c94e3357ad6e4a1c97
smmakowski <makowski.sm@gmail.com>
2017-02-23 12:23:48 -0800

    Make progress on searchYouTube.js, may need to modify tests

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,19 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: options,
+    //key: options.key,
+    //q: options.query,
+    //maxResults: options.max,
+    success: (data) => {
+      console.log(data);
+      callback(data.items);
+    },
+    error: (data) => {
+      console.log('ajax call failed' + data);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
