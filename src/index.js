9484b64048d8b378814cd75705c4e471e687d04c
smmakowski <makowski.sm@gmail.com>
2017-02-23 20:36:10 -0800

    All Tests Passing, Added Autoplay Toggler, Added Debouncer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 // TODO: Render the `App` component to the DOM
 window.autoplay = 1;
-ReactDOM.render(<App searchYouTube={window.searchYouTube} API_KEY={window.YOUTUBE_API_KEY}/>, document.getElementById('app')); 
+ReactDOM.render(<App searchYouTube={_.debounce(window.searchYouTube, 500)} API_KEY={window.YOUTUBE_API_KEY}/>, document.getElementById('app')); 
